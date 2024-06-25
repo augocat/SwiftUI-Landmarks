9148740270b5d0aaa4b74c13b66d260c64d97da5
narb <narborough.grahame@icloud.com>
2024-06-25 14:24:02 +1000

    The row view

--- Landmarks/Model/LandmarkRow.swift
+++ Landmarks/Model/LandmarkRow.swift
@@ -4,8 +4,15 @@ struct LandmarkRow: View {
   var landmark: Landmark
 
   var body: some View {
-        Text("Hello, World!")
+    HStack {
+      landmark.image
+          .resizable()
+          .frame(width: 50, height: 50)
+      Text(landmark.name)
+
+      Spacer()
     }
+  }
 }
 
 #Preview {
