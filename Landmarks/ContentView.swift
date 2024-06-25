c30f7f3bfad4ea1640c15c1bbf4560d09029c46d
narb <narborough.grahame@icloud.com>
2024-06-25 12:03:30 +1000

    A.CreatingAndCombiningViews

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -1,21 +1,35 @@
-//
-//  ContentView.swift
-//  Landmarks
-//
-//  Created by Grahame Narborough on 24/6/2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+      VStack {
+        MapView()
+          .frame(maxHeight: 300)
+        
+        CircleImage()
+          .offset(y: -130)
+          .padding(.bottom, -130)
+        
+        VStack(alignment: .leading) {
+          Text("Turtle Rock")
+            .font(.title)
+          HStack {
+            Text("Joshua Tree National Park")
+            Spacer()
+            Text("California")
+          }
+          .font(.subheadline)
+          .foregroundStyle(.secondary)
+
+          Divider()
+
+          Text("About Turtle Rock")
+              .font(.title2)
+          Text("Descriptive text goes here.")
         }
         .padding()
+      }
+      Spacer()
     }
 }
 
